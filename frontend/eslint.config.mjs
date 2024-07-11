a79dc09bf2d20322845b827eec3f0aff6b7e077a
Brian Kam <brianhyunkam@gmail.com>
2024-07-10 21:01:19 -0700

    initial setup

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      js: pluginJs.configs.recommended,
+      react: pluginReactConfig,
+    },
+  },
+];
